ee76ffa36c2dc60cf3723956fc511d0ed65d38d7
Zilio-Desk <henriquezilio4@gmail.com>
2026-06-10 13:53:49 -0300

    sweetalert2 dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,7 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 
-app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL,
-    'http://localhost:5173'
-  ]
-}))
+app.use(cors())
 
 const DBconfig = mysql.createPool({
     connectionLimit: process.env.DATABASE_CONNECTION_LIMIT,
